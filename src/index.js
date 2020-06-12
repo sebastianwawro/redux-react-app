49d112cfdf1b3040b234c8e36cf2b03589bdbad8
Sebastian Wawro <user6917@gmail.com>
2020-06-12 15:21:04 +0200

    add redux & default files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+
+ReactDOM.render(
+    <p>Hello World!</p>,
+    document.getElementById('root')
+);
